9a9b22ddb3ea2bf41854506b2865adb53e4f146e
Donatas Rasiukevicius <donatas@fly.io>
2024-05-15 12:15:12 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
