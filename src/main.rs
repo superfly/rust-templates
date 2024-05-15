51468260eaee7db3d2553544131364c8659246a9
Donatas Rasiukevicius <donatas@fly.io>
2024-05-15 13:10:26 +0200

    change response to match other apps

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "Hello from fly.io!" }));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
