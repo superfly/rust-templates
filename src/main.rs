03674ab93fd95f76bb825aac7a08dd65174acbca
Donatas Rasiukevicius <donatas@fly.io>
2024-05-15 12:36:03 +0200

    add poem quickstart

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use axum::{routing::get, Router};
+use poem::{get, handler, listener::TcpListener, Route, Server};
+
+#[handler]
+fn hello() -> String {
+    format!("hello from fly.io!")
+}
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/", get(hello));
+    Server::new(TcpListener::bind("0.0.0.0:8080"))
+        .run(app)
+        .await
 }
